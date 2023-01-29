75cdd712362657784968042b51e645fef158af53
ajp84 <prater.anthony.j@gmail.com>
2023-01-29 11:34:00 -0800

    loading custom fonts

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,27 @@
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: url('/assets/fonts/Moonhouse.ttf') format('truetype');
+  src: url('./assets/fonts/Moonhouse.ttf') format('truetype');
 }
 @font-face {
   font-family: 'Galaxy';
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: local('Galaxy'),
-    url('/assets/fonts/SFDistantGalaxyOutline.ttf') format('truetype');
+  src: url('./assets/fonts/SFDistantGalaxyOutline.ttf') format('truetype');
 }
 @font-face {
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: url('/assets/fonts/Poppins-Medium.ttf') format('truetype');
+  src: url('./assets/fonts/Poppins-Medium.ttf') format('truetype');
 }
 @font-face {
   font-family: 'PoppinsBold';
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: url('/assets/fonts/Poppins-Bold.ttf') format('truetype');
+  src: url('./assets/fonts/Poppins-Bold.ttf') format('truetype');
 }
 </style>
